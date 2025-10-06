f486896a9a10a9214c91b1f942ee43b3386fd054
Mohamed El-Sayed <137812846+mohamed3ssal@users.noreply.github.com>
2025-10-06 06:02:31 +0300

    folder structure added

---
+++ template.sh
@@ -0,0 +1,15 @@
+#creating directories
+mkdir src
+mkdir research
+
+#creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Directories and files structure created successfully."
