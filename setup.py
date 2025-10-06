40f3bfc5176496fd91f19d9494eb6819aea212f0
Mohamed El-Sayed <137812846+mohamed3ssal@users.noreply.github.com>
2025-10-06 07:06:43 +0300

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    description="A medical chatbot application using LangChain and Flask",
+    author="Mohamed Assal",
+    author_email="mohamedassal52003@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
